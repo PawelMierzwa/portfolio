fa5294bfc624f96fe94069c0db2b22a7e8b8cbb9
Paweł Mierzwa <pawelmierzwa150@gmail.com>
2023-11-05 12:41:35 +0100

    Update LinkedIn url

--- mobile/src/components/ContactSection.vue
+++ mobile/src/components/ContactSection.vue
@@ -4,7 +4,7 @@
             <h1>Contact</h1>
             <ul id="about-links">
                 <li>
-                    <a href="https://www.linkedin.com/in/paweł-mierzwa/">
+                    <a href="https://www.linkedin.com/in/cnsh/">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" role="img"
                             aria-labelledby="LinkedinIcoTitle" fill="#362e2b" class="ico">
                             <title id="LinkedinIcoTitle">My LinkedIn</title>
